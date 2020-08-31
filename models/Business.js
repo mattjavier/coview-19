d08cef081307da13f453ae34e4254b3b63ac87e6
brian <brianra2nil@hotmail.com>
2020-08-31 13:01:24 -0700

    updated

--- models/Business.js
+++ models/Business.js
@@ -16,17 +16,6 @@ Business.init({
     type: DataTypes.FLOAT,
     allowNull: false
   },
-<<<<<<< HEAD
-  averageMask: {
-    type: DataTypes.FLOAT,
-    allowNull: false
-  },
-  averageSanitation: {
-    type: DataTypes.FLOAT,
-    allowNull: false
-  },
-  averageSocialdistance: {
-=======
   avgMaskRating: {
     type: DataTypes.FLOAT,
     allowNull: false
@@ -36,7 +25,6 @@ Business.init({
     allowNull: false
   },
   avgSocialDistance: {
->>>>>>> 08251c894b5e549afcb54adef6879117b15e59a3
     type: DataTypes.FLOAT,
     allowNull: false
   }
