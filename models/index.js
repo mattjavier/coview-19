08251c894b5e549afcb54adef6879117b15e59a3
Matt Javier <matthewrjavier@gmail.com>
2020-08-31 11:02:10 -0700

    index models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
-const Business = requrie('./Business.js')
+const Business = require('./Business.js')
 const Rating = require('./Rating.js')
 const User = require('./User.js')
+
+User.hasMany(Rating)
+Business.hasMany(Rating)
+
+Rating.belongsTo(User)
+Rating.belongsTo(Business)
+
+module.exports = { Business, Rating, User}
