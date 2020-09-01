b49b3f98c74290bb0783bdfd173e3d917756d614
Matt Javier <matthewrjavier@gmail.com>
2020-09-01 14:08:00 -0700

    stars

--- public/assets/writeReview.js
+++ public/assets/writeReview.js
@@ -1,57 +1,42 @@
 $(document).ready(function() {
-  
+
+  // $('#businessType').click(function() {
+  //   $.ajax('api/businesses/types')
+  //     .then(({ data }) => {
+  //       data.forEach(type => {
+
+  //       })
+  //     })
+  // })
+
+  $("#mask").rateYo({
+    rating: 1.5,
+    halfStar: true
+  })
+
+  $("#social").rateYo({
+    rating: 1.5,
+    halfStar: true
+  })
+
+  $("#sanitation").rateYo({
+    rating: 1.5,
+    halfStar: true
+  })
+
+  $("#overall").rateYo({
+    rating: 1.5,
+    halfStar: true
+  })
+
+  $('#submit').click(function() {
+    let maskRating = $('#mask').rateYo().rateYo('rating')
+    let socialRating = $('#social').rateYo().rateYo('rating')
+    let sanitationRating = $('#sanitation').rateYo().rateYo('rating')
+    let overallRating = $('#overall').rateYo().rateYo('rating')
+    console.log(maskRating, socialRating, sanitationRating, overallRating)
+
+    // $.post('/api/ratings', { name, type, overallRating, maskRating, sanitationRating, socialRating, comment, userId, businessId  })
+  })
 })
-$(function () {
- 
- $("#mask").rateYo({
-   rating: 1.5,
-   halfStar: true
-   
- });
-});
 
-
-
-
-
-
-
-$(function () {
- 
- $("#social").rateYo({
-   rating: 1.5,
-   halfStar: true
-   
- });
-});
-
-
-
-
-
-
-$(function () {
- 
- $("#sanitation").rateYo({
-   rating: 1.5,
-   halfStar: true
-   
- });
-});
-
-
-
-
-
-
-  $(function () {
-   
-   $("#overall").rateYo({
-     rating: 1.5,
-     halfStar: true
-     
-   });
-  });
-  
-  
-  
