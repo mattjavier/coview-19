d7a797bafa52d409e97f9d145c2984b8424d9ea1
Pat Downer <patricia.downer@gmail.com>
2020-09-02 16:00:08 -0700

    assigns a variable for values of each search field

--- public/assets/viewReviews.js
+++ public/assets/viewReviews.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $.ajax('/api/businesses')
     .then(businesses => {
       businesses = businesses.map(business => business.name)
@@ -7,6 +7,23 @@ $(document).ready(function() {
         source: businesses
       })
     })
+})
+
+document.getElementById('srcBusiness').addEventListener('click', event => {
+  event.preventDefault()
+
+  let name = document.getElementById('businessName').value
+
+  let type = document.getElementById('typeSrc').options[document.getElementById('typeSrc').selectedIndex].value
+
+  let city = document.getElementById('citySrc').value
+
+  let state = document.getElementById('stateSrc').options[document.getElementById('stateSrc').selectedIndex].value
+
+  console.log(name, type, city, state)
+
+
+
 })
 
 // include jquery cdn and css
