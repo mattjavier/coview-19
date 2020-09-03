64fc1b9ee82f734d328317dd9d82b682451e9bcd
Danielle Hillman <hillm108@mail.chapman.edu>
2020-09-03 14:07:30 -0700

    pat

--- db/index.js
+++ db/index.js
@@ -1,7 +1,3 @@
 const Sequelize = require('sequelize')
 
-<<<<<<< HEAD
 module.exports = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost/coview_db')
-=======
-module.exports = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost/coview_db')
->>>>>>> fcc8b23af0454e855d7aaaaf0adb437fb653c7c4
